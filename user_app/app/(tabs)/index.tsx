d731220b17ee434131cf3479c8c144bffa9a6054
Saumy Anand <samcool3203@gmail.com>
2025-03-17 20:58:59 +0530

    login and verify otp components added

--- user_app/app/(tabs)/index.tsx
+++ user_app/app/(tabs)/index.tsx
@@ -1,49 +1,11 @@
 import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import FoodCard from '../components/Home/FoodCard';
+import Login from '../(auth)/login';
+import VerifyOtp from '../(auth)/VerifyOtp';
 
 const Index = () => {
   return (
-    <ScrollView className="flex-1 bg-white px-4 pt-6">
-      {/* Header */}
-      <View className="flex-row items-center justify-between mb-4">
-        <TouchableOpacity className="p-2">
-          <View className="w-6 h-6 bg-gray-700 rounded" />
-        </TouchableOpacity>
-        <Text className="text-lg font-semibold">Ontario, Ohio ▼</Text>
-        <TouchableOpacity className="p-2">
-          <View className="w-6 h-6 bg-gray-700 rounded-full" />
-        </TouchableOpacity>
-      </View>
-
-      {/* Search Bar */}
-      <View className="flex-row items-center bg-gray-100 rounded-lg p-3 mb-4">
-        <View className="w-5 h-5 bg-gray-500 rounded mr-2" />
-        <TextInput
-          placeholder="Restaurant, item & r"
-          placeholderTextColor="gray"
-          className="flex-1 text-base"
-        />
-        <TouchableOpacity>
-          <View className="w-5 h-5 bg-gray-500 rounded" />
-        </TouchableOpacity>
-      </View>
-
-      {/* Categories */}
-      <View className="flex-row justify-between mb-4">
-        {["Food", "Grocery", "Pharmacy"].map((category, index) => (
-          <TouchableOpacity key={index} className="items-center">
-            <View className="w-16 h-16 bg-gray-700 rounded-lg mb-2" />
-            <Text className="text-sm">{category}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
-      <FoodCard />
-      <FoodCard />
-      <FoodCard />
-      <FoodCard />
-      <FoodCard />
-    </ScrollView>
+    <VerifyOtp/>
   );
 };
 
